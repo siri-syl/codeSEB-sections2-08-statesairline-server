84fd8d6516dfb14642f953ba3686247b844e4e35
siri-syl <sylee010101@gmail.com>
2021-09-12 21:40:16 +0900

    clear

--- statesairline/controller/bookController.js
+++ statesairline/controller/bookController.js
@@ -7,16 +7,26 @@ module.exports = {
   // 전체 데이터 혹은 요청 된 flight_uuid, phone 값과 동일한 예약 데이터를 조회합니다.
   findById: async (req, res) => {
     //TODO: 
-    
+    // {
+    //   flight_uuid: 'af6fa55c-da65-47dd-af23-578fdba44bed',
+    //   name: '김코딩',
+    //   phone: '010-1234-5678'
+    // }
+    let reqQue = req.query,
+    keys = Object.keys(reqQue);
+    let result = booking.filter((bookItem) => keys.every(k =>  reqQue[k] === bookItem[k]));
 
-    return res.status(200).json(booking);
+    if(!!reqQue.phone){
+      return res.status(200).json(result[0]);
+    }
+    return res.status(200).json(result);
   },
 
   // [POST] /book 요청을 수행합니다.
   // 요청 된 예약 데이터를 저장합니다.
   create: async (req, res) => {
     //TODO: 
-    
+    booking.push(req.body)
     return res.status(201).json({ message: 'Create success!' });
   },
 
@@ -24,9 +34,14 @@ module.exports = {
   // 요청 된 phone 값과 동일한 예약 데이터를 삭제합니다.
   deleteById: async (req, res) => {
     //TODO: 
-
+    let reqQue = req.query;
+    let result = [];
+    for(let i=0; i<booking.length; i++){
+      if(booking[i].phone === reqQue.phone){
+        result = booking.slice(0,i).concat(booking.slice(i+1));
+      }
+    }
     
-    
-    return res.status(200).json(booking);
+    return res.status(200).json(result);
   }
 };
--- statesairline/controller/flightController.js
+++ statesairline/controller/flightController.js
@@ -1,3 +1,4 @@
+const { filter } = require('../repository/flightList');
 const flights = require('../repository/flightList');
 
 module.exports = {
@@ -5,28 +6,65 @@ module.exports = {
   // 요청 된 departure_times, arrival_times, destination, departure 값과 동일한 값을 가진 항공편 데이터를 조회합니다.
   findAll: async (req, res) => {
     //TODO: 
+    // /flight?departure_times=2021-12-03T12:00:00&arrival_times=2021-12-03T12:00:00
+    // /flight?departure=CJU&destination=ICN
 
+    let reqQue = req.query;
+    // console.log(reqQue);
+    let keys = Object.keys(reqQue),
+    result = flights.filter(flightItem => keys.every(k => reqQue[k] === flightItem[k]));
 
+    // let result = flights.filter((item) => {
+    //   if(reqQue.length !== 0) return true;
+    //   else{
+    //     for(let prop in reqQue){
 
-    return res.json(flights);
+    //       flights[prop] === reqQue[prop]
+    //     }
+    //   }
+    // })
+
+    return res.json(result);
   },
   // [GET] /flight/{:id}
   // 요청 된 id 값과 동일한 uuid 값을 가진 항공편 데이터를 조회합니다.
   findById: async (req, res) => {
     //TODO: 
-    
+    let reqPar = req.params;
+    let result = flights.filter((item) => {
+      if(reqPar.length === 0) {
+        return true;
+      }
+      else{
+        return item.uuid === reqPar.id
+      }
+    })
 
-
+    return res.json(result)
   },
 
   // [PUT] /flight/{:id} 요청을 수행합니다.
   // 요청 된 id 값과 동일한 uuid 값을 가진 항공편 데이터를 요청 된 Body 데이터로 수정합니다.
   update: async (req, res) => {
-    let data;
+    let data = {};
+    let tempFlights = flights.slice();
     //TODO: 
 
-    
+    let reqBody = req.body; // 바꿔야 하는 정보
+    let reqPar = req.params; // 요청 id값
 
+    for(let i=0; i<tempFlights.length; i++){
+      if(flights[i].uuid === reqPar.id){
+        for(let prop in tempFlights[i]){
+          data[prop] = tempFlights[i][prop];
+        }
+        for(let prop in reqBody){
+          data[prop] = reqBody[prop];
+          tempFlights[i][prop] = reqBody[prop];
+        }
+      }
+    }
+    // console.log(data)
     
     return res.status(200).json(data);
   }
